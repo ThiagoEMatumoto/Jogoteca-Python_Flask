df5daac9901da12c068927f4f022751b1300ea15
ThiagoEMatumoto <thiagoeijimatumoto@gmail.com>
2022-10-08 17:00:51 -0300

    adding the game class and manipulating the list

--- jogoteca.py
+++ jogoteca.py
@@ -1,11 +1,23 @@
+from mimetypes import init
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+class jogo:
+    def __init__(self, nome, empresa, console):
+        self.nome = nome
+        self.empresa = empresa
+        self.console = console
 
 @app.route('/inicio')
 def ola():
-    lista = ['Valorant', 'League of Legends', 'Overwatch 2', 'Fortnite']
+    valorant = jogo(nome='Valorant', empresa='Riot', console='Pc' )
+    league_of_legends = jogo(nome="League of Legends", empresa='Riot', console='Pc')
+    overwath2 = jogo(nome='Overwatch 2', empresa='Blizzard', console='Pc')
+    fortnite = jogo(nome='Fortnite', empresa='EpicGames', console='Multiplataforma')
+    
+    
+    lista = [valorant, league_of_legends, overwath2, fortnite]
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 
