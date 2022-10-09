05caacf1ed37156611b4806e2e4664341e3f2665
ThiagoEMatumoto <thiagoeijimatumoto@gmail.com>
2022-10-08 22:02:38 -0300

    Adicionando o método PUSH para a adicionar um novo jogo

--- jogoteca.py
+++ jogoteca.py
@@ -1,7 +1,7 @@
-from mimetypes import init
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+
 
 class jogo:
     def __init__(self, nome, empresa, console):
@@ -9,16 +9,33 @@ class jogo:
         self.empresa = empresa
         self.console = console
 
+
+valorant = jogo(nome='Valorant', empresa='Riot', console='Pc')
+league_of_legends = jogo(nome="League of Legends", empresa='Riot', console='Pc')
+overwath2 = jogo(nome='Overwatch 2', empresa='Blizzard', console='Pc')
+fortnite = jogo(nome='Fortnite', empresa='EpicGames', console='Multiplataforma')
+
+lista = [valorant, league_of_legends, overwath2, fortnite]
+
+
 @app.route('/inicio')
 def ola():
-    valorant = jogo(nome='Valorant', empresa='Riot', console='Pc' )
-    league_of_legends = jogo(nome="League of Legends", empresa='Riot', console='Pc')
-    overwath2 = jogo(nome='Overwatch 2', empresa='Blizzard', console='Pc')
-    fortnite = jogo(nome='Fortnite', empresa='EpicGames', console='Multiplataforma')
-    
-    
-    lista = [valorant, league_of_legends, overwath2, fortnite]
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 
-app.run()
+@app.route('/novo_jogo')
+def novo_jogo():
+    return render_template('novo_jogo.html', titulo='Cadastro de jogos')
+
+
+@app.route('/criar_jogo', methods=['POST', ])
+def criar_jogo():
+    nome = request.form['nome']
+    empresa = request.form['empresa']
+    console = request.form['console']
+    new_jogo = jogo(nome, empresa, console)
+    lista.append(new_jogo)
+    return redirect('/')
+
+
+app.run(debug=True)
