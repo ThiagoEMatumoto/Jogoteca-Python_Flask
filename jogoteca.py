60c476174bec7cb05e2df68df5dfd775d9ea12f7
ThiagoEMatumoto <thiagoeijimatumoto@gmail.com>
2022-10-10 18:50:16 -0300

    arrumando erro de login, criando idex e app.route /

--- jogoteca.py
+++ jogoteca.py
@@ -4,19 +4,24 @@ app = Flask(__name__)
 app.secret_key = 'criptografado'
 
 
-class jogo:
+class Jogo:
     def __init__(self, nome, empresa, console):
         self.nome = nome
         self.empresa = empresa
         self.console = console
 
 
-valorant = jogo(nome='Valorant', empresa='Riot', console='Pc')
-league_of_legends = jogo(nome="League of Legends", empresa='Riot', console='Pc')
-overwath2 = jogo(nome='Overwatch 2', empresa='Blizzard', console='Pc')
-fortnite = jogo(nome='Fortnite', empresa='EpicGames', console='Multiplataforma')
+valorant = Jogo(nome='Valorant', empresa='Riot', console='Pc')
+league_of_legends = Jogo(nome="League of Legends", empresa='Riot', console='Pc')
+overwath2 = Jogo(nome='Overwatch 2', empresa='Blizzard', console='Pc')
+fortnite = Jogo(nome='Fortnite', empresa='EpicGames', console='Multiplataforma')
 
 lista = [valorant, league_of_legends, overwath2, fortnite]
+
+
+@app.route('/')
+def index():
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 
 @app.route('/inicio')
@@ -34,7 +39,7 @@ def criar_jogo():
     nome = request.form['nome']
     empresa = request.form['empresa']
     console = request.form['console']
-    new_jogo = jogo(nome, empresa, console)
+    new_jogo = Jogo(nome, empresa, console)
     lista.append(new_jogo)
     return redirect('/')
 
@@ -44,15 +49,22 @@ def login():
     return render_template('login.html')
 
 
-@app.route('/autenticar', methods=['POST', ])
+@app.route('/autenticar', methods=['POST',])
 def autenticar():
-    if 'alohomora' == request.form['senha']:
+    if 'teste' == request.form['senha']:
         session['usuario_logado'] = request.form['usuario']
-        flash(request.form['usuario'] + ' logou com sucesso!')
+        flash(session['usuario_logado'] + ' logado com sucesso!')
         return redirect('/')
     else:
         flash('Usuário não logado.')
         return redirect('/login')
+
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Logout realizado com sucesso!')
+    return redirect('/')
 
 
 app.run(debug=True)
