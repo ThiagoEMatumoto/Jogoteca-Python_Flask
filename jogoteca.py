426afd7f133d10e445fcd91812238ecbc478459c
ThiagoEMatumoto <thiagoeijimatumoto@gmail.com>
2022-10-16 16:47:37 -0300

    Adicionando banco de dados com MySQL e refatoração do código

--- jogoteca.py
+++ jogoteca.py
@@ -1,71 +1,82 @@
 from flask import Flask, render_template, request, redirect, session, flash, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.secret_key = 'criptografado'
+app.secret_key = 'alura'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
+        SGBD='mysql+mysqlconnector',
+        usuario='root',
+        senha='Sky1Thi@goS',
+        servidor='localhost',
+        database='jogoteca'
+    )
+
+db = SQLAlchemy(app)
 
 
-class Usuario:
-    def __init__(self, nome, nickname, senha):
-        self.nome = nome
-        self.nickname = nickname
-        self.senha = senha
+class Jogos(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String(50), nullable=False)
+    categoria = db.Column(db.String(40), nullable=False)
+    console = db.Column(db.String(20), nullable=False)
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
 
 
-usuario_mestre = Usuario('teste', 'teste', 'teste')
-thiago = Usuario('Thiago', 'Thiago', '1234')
+class Usuarios(db.Model):
+    nickname = db.Column(db.String(8), primary_key=True)
+    nome = db.Column(db.String(20), nullable=False)
+    senha = db.Column(db.String(100), nullable=False)
 
-usuarios = {usuario_mestre.nickname: usuario_mestre,
-            thiago.nickname: thiago}
-
-
-class Jogo:
-    def __init__(self, nome, empresa, console):
-        self.nome = nome
-        self.empresa = empresa
-        self.console = console
-
-
-valorant = Jogo(nome='Valorant', empresa='Riot', console='Pc')
-league_of_legends = Jogo(nome="League of Legends", empresa='Riot', console='Pc')
-overwath2 = Jogo(nome='Overwatch 2', empresa='Blizzard', console='Pc')
-fortnite = Jogo(nome='Fortnite', empresa='EpicGames', console='Multiplataforma')
-
-lista = [valorant, league_of_legends, overwath2, fortnite]
+    def __repr__(self):
+        return '<Name %r>' % self.name
 
 
 @app.route('/')
 def index():
+    lista = Jogos.query.order_by(Jogos.id)
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 
-@app.route('/novo_jogo')
-def novo_jogo():
+@app.route('/novo')
+def novo():
     if 'usuario_logado' not in session or session['usuario_logado'] is None:
-        return redirect(url_for('login', proxima=url_for('novo_jogo')))
-    else:
-        return render_template('novo_jogo.html', titulo='Cadastro de jogos')
+        return redirect(url_for('login', proxima=url_for('novo')))
+    return render_template('novo.html', titulo='Novo Jogo')
 
 
-@app.route('/criar_jogo', methods=['POST', ])
-def criar_jogo():
+@app.route('/criar', methods=['POST', ])
+def criar():
     nome = request.form['nome']
-    empresa = request.form['empresa']
+    categoria = request.form['categoria']
     console = request.form['console']
-    new_jogo = Jogo(nome, empresa, console)
-    lista.append(new_jogo)
+
+    jogo = Jogos.query.filter_by(nome=nome).first()
+
+    if jogo:
+        flash('Jogo já existente!')
+        return redirect(url_for('index'))
+
+    novo_jogo = Jogos(nome=nome, categoria=categoria, console=console)
+    db.session.add(novo_jogo)
+    db.session.commit()
+
     return redirect(url_for('index'))
 
 
 @app.route('/login')
 def login():
-    proxima_pagina = request.args.get('proxima')
-    return render_template('login.html', proxima=proxima_pagina)
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)
 
 
 @app.route('/autenticar', methods=['POST', ])
 def autenticar():
-    if request.form['usuario'] in usuarios:
-        usuario = usuarios[request.form['usuario']]
+    usuario = Usuarios.query.filter_by(nickname=request.form['usuario']).first()
+    if usuario:
         if request.form['senha'] == usuario.senha:
             session['usuario_logado'] = usuario.nickname
             flash(usuario.nickname + ' logado com sucesso!')
@@ -78,9 +89,9 @@ def autenticar():
 
 @app.route('/logout')
 def logout():
-        session['usuario_logado'] = None
-        flash('Logout realizado com sucesso!')
-        return redirect(url_for('index'))
+    session['usuario_logado'] = None
+    flash('Logout efetuado com sucesso!')
+    return redirect(url_for('index'))
 
 
 app.run(debug=True)
